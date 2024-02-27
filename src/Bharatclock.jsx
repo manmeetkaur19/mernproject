cfb6fe7ceb8ffe5bbfab14e9bcb906e6394ce75f
manmeetkaur19 <manmeetkaur16925@gmail.com>
2024-02-26 22:27:03 -0800

    project

---
+++ src/Bharatclock.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Bharatclock() {
+  return (
+    <div className='row'>
+      <div class='>Bharatclock'>
+    <div>Bharatclock
+    <p> This is the clock that shows the time in Bharat at all times.</p>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+
