044660ba9a72e3778f4db4955e9435bcbb05da50
manmeetkaur19 <manmeetkaur16925@gmail.com>
2024-02-26 22:41:29 -0800

    project

--- src/Aboutus.jsx
+++ src/Aboutus.jsx
@@ -2,8 +2,67 @@
 // component making
 import React from 'react';
 
-export default function Aboutus() {
+export default function Aboutus() 
+{
   return (
-    <div>welcome to react</div>
+    <div className='row'>
+    <div className="card col mx-1"> 
+     <div class={`card`}> 
+  <img src="python.jpg" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">python language</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="https://www.google.com/search?q=python+programming&rlz=1C1RXQR_enIN1098IN1098&oq=&gs_lcrp=EgZjaHJvbWUqCQgAEEUYOxjCAzIJCAAQRRg7GMIDMgkIARBFGDsYwgMyCQgCEEUYOxjCAzIJCAMQRRg7GMIDMgkIBBBFGDsYwgMyCQgFEEUYOxjCAzIJCAYQRRg7GMIDMgkIBxBFGDsYwgPSAQozNTUyMTRqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8" class="btn btn-primary">Explore More</a>
+  </div>
+  </div>
+   </div> 
+
+    <div className="class col mx-1"> 
+     <div class={'card'}> 
+  <img src="Clanguage.png" class="card-img-top"  alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">C language</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="https://www.w3schools.com/c/c_intro.php?external_link=true" class="btn btn-primary">Explore More</a>
+  </div>
+  </div>
+  </div> 
+
+
+ <div className="class col mx-1">
+ <div class={'card'}>
+<img src="C++language.png" class="card-img-top" alt="..."></img>
+<div class="card-body">
+<h5 class="card-title">C++ language</h5>
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<a href="https://www.google.com/search?q=c%2B%2B+programming&sca_esv=476da7e8abe42804&rlz=1C1RXQR_enIN1098IN1098&ei=qcXcZb_FD6DU4-EPwN2o8A0&ved=0ahUKEwj_vce4v8mEAxUg6jgGHcAuCt4Q4dUDCBA&uact=5&oq=c%2B%2B+programming&gs_lp=Egxnd3Mtd2l6LXNlcnAiD2MrKyBwcm9ncmFtbWluZzIQEAAYgAQYigUYQxixAxiDATIKEAAYgAQYigUYQzINEAAYgAQYigUYQxixAzIFEAAYgAQyChAAGIAEGIoFGEMyChAAGIAEGIoFGEMyChAAGIAEGIoFGEMyBRAAGIAEMgoQABiABBiKBRhDMgoQABiABBiKBRhDSMYmUNoEWLAdcAF4AJABAZgB_wGgAYMOqgEFMC41LjS4AQPIAQD4AQGYAgagAqAIwgIKEAAYRxjWBBiwA8ICDRAAGIAEGIoFGEMYsAPCAgYQABgHGB7CAggQABiABBixA8ICChAuGIAEGIoFGEPCAgsQABiABBixAxiDAcICDRAuGIAEGIoFGEMY1ALCAgcQABiABBgKmAMAiAYBkAYKkgcFMS4yLjM&sclient=gws-wiz-serp" class="btn btn-primary">Explore More</a>
+</div>
+</div>
+</div>
+
+ <div className="class col mx-1"> 
+ <div class={'card'}> 
+<img src="javalang.png" class="card-img-top" alt="..."></img>
+<div class="card-body">
+<h5 class="card-title">JAVA language</h5>
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<a href="https://www.google.com/search?q=java+programming&sca_esv=476da7e8abe42804&rlz=1C1RXQR_enIN1098IN1098&ei=BMbcZb6WDu2R4-EP5sSJ4AU&ved=0ahUKEwi-qPjjv8mEAxXtyDgGHWZiAlwQ4dUDCBA&uact=5&oq=java+programming&gs_lp=Egxnd3Mtd2l6LXNlcnAiEGphdmEgcHJvZ3JhbW1pbmdIAFAAWABwAHgAkAEAmAEAoAEAqgEAuAEDyAEAmAIAoAIAmAMAkgcA&sclient=gws-wiz-serp" class="btn btn-primary">Explore More</a>
+</div>
+</div>
+ </div> 
+
+ <div className="class col mx-1"> 
+ <div class={'card'}> 
+<img src="CSharplang.png" class="card-img-top" alt="..."></img>
+<div class="card-body">
+<h5 class="card-title">C Sharp language</h5>
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<a href="https://www.google.com/search?q=c+sharp+programming&sca_esv=476da7e8abe42804&rlz=1C1RXQR_enIN1098IN1098&ei=k8bcZa_BFsmXvr0Plr2riAI&ved=0ahUKEwjv1piowMmEAxXJi68BHZbeCiEQ4dUDCBA&uact=5&oq=c+sharp+programming&gs_lp=Egxnd3Mtd2l6LXNlcnAiE2Mgc2hhcnAgcHJvZ3JhbW1pbmcyCxAAGIAEGLEDGIMBMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBhAAGAcYHjIGEAAYBxgeMgUQABiABDIGEAAYBxgeSIsyUKwEWMQlcAF4AJABAJgBvQOgAZIdqgEJMC4yLjQuMy4zuAEDyAEA-AEBmAIJoALVEMICChAAGEcY1gQYsAPCAg0QABiABBiKBRhDGLADwgINEAAYgAQYigUYQxixA8ICChAAGIAEGIoFGEPCAgoQLhiABBiKBRhDwgIIEAAYgAQYsQPCAhAQABiABBiKBRhDGLEDGIMBwgINEC4YgAQYigUYQxjUAsICExAAGIAEGA0YsQMYgwEYsQMYgwHCAg0QABiABBgNGLEDGIMBwgIHEAAYgAQYDcICChAAGIAEGA0YsQOYAwCIBgGQBgqSBwkxLjEuNS4xLjE&sclient=gws-wiz-serp" class="btn btn-primary">Explore More</a>
+</div>
+</div>
+</div>
+</div>
+
   )
 }
+  
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Aboutus from './Aboutus'
+import Bharatclock from './Bharatclock'
 
 export default function App() {
   return (
     <div>
       <Aboutus/>
     </div>
+    
   )
 }
